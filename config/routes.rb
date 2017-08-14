a2f32d9821c79e19874b61d7ddd0827ef115899c
bentleyaustin86 <bentleyaustin86@gmail.com>
2017-08-14 09:30:54 -0600

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  get 'pages_controller/home'
+
+  get 'pages_controller/profile'
+
+  get 'pages_controller/news'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
